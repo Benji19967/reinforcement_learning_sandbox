02c9be17fdd87426f3388000b6cb58cd0e740929
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-06-01 14:12:56 +0200

    Add interpreter path for vscode

--- rl/main.py
+++ rl/main.py
@@ -4,10 +4,11 @@ env = gym.make("CartPole-v1", render_mode="human")
 # env = gym.make("LunarLander-v2", render_mode="human")
 
 observation, info = env.reset(seed=42)
-for _ in range(1000):
+for _ in range(50):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
+    print(observation, reward, terminated, truncated, info)
 
-    if terminated or truncated:
-        observation, info = env.reset()
+    # if terminated or truncated:
+    #     observation, info = env.reset()
 env.close()
